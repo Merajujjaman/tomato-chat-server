77edb88567d717df643f3303614721723d9ae58e
Merajujjaman Meraj <asmmerajujjaman@gmail.com>
2025-06-10 15:44:05 +0600

    authentication setup for backend done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const chatRoutes = require('./routes/chatRoutes');
+const authRoutes = require('./routes/authRoutes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api/chat', chatRoutes);
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
