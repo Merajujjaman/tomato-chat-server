c168cbdc40c7e7e69ae9539c792be9e1d83cf5df
Merajujjaman Meraj <asmmerajujjaman@gmail.com>
2025-06-16 22:07:59 +0600

    fixed some issu

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const socketIo = require('socket.io');
-const Message = require('../models/message');
 
 const setupSocket = (app) => {
     const server = http.createServer(app);
@@ -9,14 +8,7 @@ const setupSocket = (app) => {
     io.on('connection', (socket) => {
         console.log('New client connected');
 
-        socket.on('sendMessage', async (data) => {
-            const message = new Message(data);
-            await message.save();
-            io.emit('receiveMessage', message);
-        });
-
-        socket.on("private message", async (msg) => {
-            // REMOVE any code here that saves the message to the database!
+        socket.on("private message", (msg) => {
             // Just broadcast to the recipient:
             io.to(msg.receiver).emit("private message", msg);
         });
